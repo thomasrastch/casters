dd49b78555a36c3d769686b574f33601556dd494
Paul Rast <pauljrast@gmail.com>
2026-04-06 16:06:15 +0200

    casting

--- scenes/level/2d/grid_display.gd
+++ scenes/level/2d/grid_display.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-var rect
 @export var x_squares = 3
 @export var y_squares = 3
 @export var square_size = 64
@@ -17,11 +16,14 @@ func _process(delta: float) -> void:
 	for player in player_container.get_children():
 		if not player.is_local_player():
 			continue
-		$ColorRect.position = Vector2(
-			floor(player.position.x / 64) * 64,
-			floor(player.position.y / 64) * 64)
-		$ColorRect.color = Color.from_hsv(
-			hash(player.get_multiplayer_authority()) / (2**63-1),
-			1-$ColorRect.position.distance_to(player.position)/64,
-			1
-		)
+		for i in range(3):
+			for j in range(3):
+				var rect = get_node("ColorRect%d%d" % [i, j])
+				rect.position = Vector2(
+					floor(player.position.x / 64 - 0.5) * 64 + (i - 1) * 64,
+					floor(player.position.y / 64 - 0.5) * 64 + (j - 1) * 64)
+				rect.color = Color.from_hsv(
+					hash(player.get_multiplayer_authority()) / (2**63-1),
+					0.5-rect.position.distance_to(player.position - Vector2(32,32))/128,
+					1
+				)
---
+++ scripts/spells/base.gd
@@ -0,0 +1,13 @@
+class_name SpellBase extends Node
+
+@export var positions: Array[Vector2]
+@export var effect_center: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
